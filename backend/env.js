3fae9100c46b8e886cdd2f66da6a9f21499dd9d6
Deepanshugupta2001 <deepanshugupta.dg.2001@gmail.com>
2026-05-11 21:27:47 +0530

    Backend URL update

--- backend/env.js
+++ backend/env.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const env = {
     CORS_ORIGIN: process.env.CORS_ORIGIN || 'zoronal-six.vercel.app',
-    PORT: process.env.PORT || 4444,
+    PORT: process.env.PORT || 'https://zoronal-9zsp.onrender.com',
     JWT_SECRET: process.env.JWT_SECRET,
     DATABASE_URL: process.env.DATABASE_URL
 }
