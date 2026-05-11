54d43ec0ddf1f275c8bc7984df14b92e508ca16f
Deepanshugupta2001 <deepanshugupta.dg.2001@gmail.com>
2026-05-11 23:07:45 +0530

    Frontend URl change

--- backend/env.js
+++ backend/env.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const env = {
-    CORS_ORIGIN: process.env.CORS_ORIGIN || 'zoronal-six.vercel.app',
+    CORS_ORIGIN: process.env.CORS_ORIGIN || 'https://zoronal-six.vercel.app',
     PORT: process.env.PORT || 'https://zoronal-9zsp.onrender.com',
     JWT_SECRET: process.env.JWT_SECRET,
     DATABASE_URL: process.env.DATABASE_URL
